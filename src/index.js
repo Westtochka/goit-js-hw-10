0f303a8886b66a1b77c521fbd7551ea6a754587d
Olga Boiko <Itnumber1@ukr.net>
2023-06-02 19:18:30 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.getElementById('breed-select');
 const catInfo = document.querySelector('.cat-info');
-
+const img= document.querySelector('.cat-image')
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
+const form = document.querySelector('form');
 
 breedSelect.addEventListener('change', handleBreedSelect);
 window.addEventListener('load', init);
-//
+
+error.style.display = 'none';
+loader.style.display = 'block';
+
 function init() {
   populateBreeds();
   breedSelect.addEventListener('change', handleBreedSelect);//слухач за вибором породи в селекті
@@ -19,14 +23,11 @@ function populateBreeds() {
     .then(breeds => {
       const markup = createOptions(breeds);
       addMarkup(markup, breedSelect);//
-      // breedSelect.removeAttribute('disabled');
       loader.style.display = 'none';
     })
     .catch(() => {
       showError();
-      loader.style.display = 'none';
-
-    });
+      });
 }
 //перебирає кожен елемент(обєкт) масиву, що прийшов і бере властивості і вставляє їх в розмітку-це масив, збирає масив в строку.
 function createOptions(data=[]){
@@ -34,40 +35,46 @@ return data
 .map(({reference_image_id, name})=>{
   return `<option value="${reference_image_id}">${name}</option>`;
 }).join('');
+
 }
 //створеня розмітки для інформації про обраного кота, 
 function createCard({
   url = '',
   breeds: [{ description = '', temperament = '', name = '' }],
 } = {}) {
-  return `<img id="cat-image" src="${url}" alt="Cat">
+  return `<img width=300 id="cat-image" src="${url}" alt="Cat">
   <div id="cat-details">
     <h2 id="description, temperamentcat-breed">${name}</h2>
     <p id="cat-description">${description}</p>
     <p id="cat-temperament"><span>Temperament:</span> ${temperament}</p>
   </div>`
+  
 }
 
 function addMarkup(markup = '', elem) {
-  elem.innerHTML = markup;
+ elem.innerHTML = markup;  
 }
-//
+
 function handleBreedSelect() {
   const breedId = breedSelect.value;
+  loader.style.display = 'block';
+
 fetchCatByBreed (breedId)
 .then(cat => {
-  const markup = createCard(cat);//
+  const markup = createCard(cat);
   addMarkup(markup, catInfo);
   error.style.display = 'none';
-  // catInfo.style.display = 'block';
   loader.style.display = 'none';
 })
 .catch(() => {
   showError();
-  loader.style.display = 'none';
+ 
 });}
+
 function showError() {
+  form.reset();
   error.style.display = 'block';
+  loader.style.display = 'none';
 }
 
-loader.style.display = 'block';
+
